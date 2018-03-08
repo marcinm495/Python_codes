56e7a747b0b8e3c093cb2b100563661b1b07ec55
marcinm495 <36198072+marcinm495@users.noreply.github.com>
2018-03-08 15:12:19 +0100

    Create Scales_test.py

---
+++ Music_Scales/Scales_test.py
@@ -0,0 +1,9 @@
+import Scales as sc
+
+scale1=sc.Scales("C","dur")
+scale1.displayScale()
+scale1.displayTriad()
+print(scale1.returnScale())
+scale2=sc.Scales("C","moll")
+scale2.displayScale()
+scale2.displayTriad()
